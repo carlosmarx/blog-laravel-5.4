d2bcc02d7c45c1676960d3dd05ed98a2a57e52fb
Carlos Marx <carlosmarx27@gmail.com>
2017-04-22 02:59:35 -0300

    Relationship Post/Comments, add comments

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['body', 'title'];
+
+    public function comments()
+    {
+      return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+      // Comment::create([
+      //       'body' => $body,
+      //       'post_id' => $this->id
+      //   ]);
+
+      //Use relationship
+      $this->comments()->create(compact('body'));
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,5 +13,36 @@
   <a class="btn btn-primary" href="{{ url('/') }}">Back to List Posts <span class="glyphicon glyphicon-chevron-right"></span></a>
 
   <hr>
+
+  <div class="comments">
+    <h2>Comments</h2>
+    <hr>
+    <ul class="list-group">
+      @foreach($post->comments as $comment)
+        <li class="list-group-item">
+          <strong>{{ $comment->created_at->diffForHumans() }}: </strong>
+          {{ $comment->body }}
+
+        </li>
+      @endforeach
+    </ul>
+  </div>
+
+  <hr>
+  {{-- Add a comment --}}
+  <div class="card">
+    <div class="card-block">
+      @include('layouts.partials.errors')
+      <form method="POST" action="/posts/{{$post->id}}/comments">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <textarea class="form-control" name="body" placeholder="Your comment here..."></textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-success">Add Comment</button>
+        </div>
+      </form>
+    </div>
+  </div>
 @endsection
 
